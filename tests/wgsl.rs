a840172f84e4d6f0f135d96ec04929f3750a01ce
MEMOPERF <contact@checkupauto.fr>
2026-08-10 18:47:13 +0200

    test(m11): validate rectangular projection shader

--- tests/wgsl.rs
+++ tests/wgsl.rs
@@ -1,6 +1,7 @@
 use flat_attention::{
     FLAT_FWD_F16_WGSL, FLAT_FWD_GROUPED_ROPE_WGSL, FLAT_FWD_GROUPED_WGSL,
-    FLAT_FWD_PROJECTION_ROPE_WGSL, FLAT_FWD_SINGLE_WGSL, FLAT_FWD_SUBGROUP_WGSL, FLAT_FWD_WGSL,
+    FLAT_FWD_PROJECTION_ROPE_ASYMMETRIC_WGSL, FLAT_FWD_PROJECTION_ROPE_WGSL,
+    FLAT_FWD_SINGLE_WGSL, FLAT_FWD_SUBGROUP_WGSL, FLAT_FWD_WGSL,
 };
 use naga::valid::{Capabilities, ShaderStages, SubgroupOperationSet, ValidationFlags, Validator};
 
@@ -44,6 +45,14 @@ fn fused_rotary_grouped_q4_shader_parses_and_validates() {
 #[test]
 fn projection_rotary_grouped_q4_shader_parses_and_validates() {
     validate_shader("Q4 projection RoPE", FLAT_FWD_PROJECTION_ROPE_WGSL);
+}
+
+#[test]
+fn asymmetric_projection_rotary_grouped_q4_shader_parses_and_validates() {
+    validate_shader(
+        "Q4 asymmetric projection RoPE",
+        FLAT_FWD_PROJECTION_ROPE_ASYMMETRIC_WGSL,
+    );
 }
 
 #[test]
