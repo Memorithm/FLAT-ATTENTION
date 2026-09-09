ab0a115f035de29ed2523e5f3290a74b587ad6d6
MEMOPERF <contact@checkupauto.fr>
2026-09-09 23:21:41 +0200

    test(kv): exercise public residency trace validator

--- tests/kv_residency_trace_contract.rs
+++ tests/kv_residency_trace_contract.rs
@@ -1,33 +1,7 @@
 use flat_attention::paged_kv::{
     KvResidencyEvent, KvResidencyEventKind, PagedKvConfig, PagedKvObservation, PagedKvTable,
+    observation::KvResidencyTraceError,
 };
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum TraceError {
-    NonMonotoneSequence,
-    PhysicalPageOutOfRange,
-    GenerationMismatch,
-}
-
-fn validate_trace(
-    observation: &PagedKvObservation,
-    events: &[KvResidencyEvent],
-) -> Result<(), TraceError> {
-    let mut previous_sequence = None;
-    for event in events {
-        if previous_sequence.is_some_and(|previous| event.sequence() <= previous) {
-            return Err(TraceError::NonMonotoneSequence);
-        }
-        if event.physical_page() >= observation.config().physical_pages {
-            return Err(TraceError::PhysicalPageOutOfRange);
-        }
-        if event.generation() != observation.telemetry().generation {
-            return Err(TraceError::GenerationMismatch);
-        }
-        previous_sequence = Some(event.sequence());
-    }
-    Ok(())
-}
 
 #[test]
 fn observation_scoped_trace_accepts_ordered_policy_neutral_events() {
@@ -45,7 +19,7 @@ fn observation_scoped_trace_accepts_ordered_policy_neutral_events() {
         KvResidencyEvent::new(12, 1, generation, KvResidencyEventKind::Promote),
     ];
 
-    assert_eq!(validate_trace(&observation, &events), Ok(()));
+    assert_eq!(observation.validate_residency_trace(&events), Ok(()));
 }
 
 #[test]
@@ -64,8 +38,8 @@ fn trace_rejects_non_monotone_sequences() {
     ];
 
     assert_eq!(
-        validate_trace(&observation, &events),
-        Err(TraceError::NonMonotoneSequence)
+        observation.validate_residency_trace(&events),
+        Err(KvResidencyTraceError::NonMonotoneSequence)
     );
 }
 
@@ -85,8 +59,8 @@ fn trace_rejects_pages_outside_declared_capacity() {
     );
 
     assert_eq!(
-        validate_trace(&observation, &[event]),
-        Err(TraceError::PhysicalPageOutOfRange)
+        observation.validate_residency_trace(&[event]),
+        Err(KvResidencyTraceError::PhysicalPageOutOfRange)
     );
 }
 
@@ -105,7 +79,7 @@ fn trace_rejects_events_from_a_stale_generation() {
     let event = KvResidencyEvent::new(1, 0, stale_generation, KvResidencyEventKind::Promote);
 
     assert_eq!(
-        validate_trace(&observation, &[event]),
-        Err(TraceError::GenerationMismatch)
+        observation.validate_residency_trace(&[event]),
+        Err(KvResidencyTraceError::GenerationMismatch)
     );
 }
